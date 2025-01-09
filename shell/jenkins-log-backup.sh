1f22df93c886dda3f4a0e73d8daa3d7e4d3057a0
krishmint <162588734+krishmint@users.noreply.github.com>
2025-01-09 16:34:02 +0530

    completed shell script

--- shell/jenkins-log-backup.sh
+++ shell/jenkins-log-backup.sh
@@ -21,5 +21,29 @@ fi
 
 ### we will need multiple "for-loop"
 # 1st "for-loop" over all jobs (diff projects)
-# 2nd "for-loop" inside 1st "for-loop" for all builds number of jobs
+# 2nd "for-loop" inside 1st "for-loop" for all builds number of todays date 
+# "if-condition" inside 2nd loop to check the buildnumber folder date  and upload only if it is of current date
+
+
+for job_dir in "$JENKINS_JOBS"*/; do
+   JOB_NAME=$(base "$job_dir")
+
+
+      for build_dir in "$job_dir/builds/"*/; do
+         BUILD_NUM=$(basename "$build_dir")
+         LOG_FILE="$build_dir/log"
+
+         # Check if log file exists and was created today
+         if [ -f "$LOG_FILE" ] && [ "$(date -r "$LOG_FILE" +%Y-%m-%d)" == "$DATE" ]; then
+         # [ -f "$LOG_FILE" ] = This checks if the file specified by the variable LOG_FILE exists and is a regular file.
+         
+             aws s3 cp "$LOG_FILE" "$S3_BUCKET/$JOB_NAME-$BUILD_NUM.log" --only-show-errors  # Upload log file to S3 with the build number as the filename
+
+             if [ $? -eq 0 ]; then ## $? :This is a special variable in Bash that holds the exit status of the last executed command
+                                   # -eq: This is a comparison operator in Bash that means "equal to.It's used to compare integers.
+             echo "Uploaded: $JOB_NAME/$BUILD_NUM to $S3_BUCKET/$JOB_NAME-$BUILD_NUM.log"
+             else
+             echo "Failed to upload: $JOB_NAME/$BUILD_NUM"
+             fi
+         fi
 
